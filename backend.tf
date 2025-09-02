266ae4026cf335055000c668b7448e646105c528
Clemens Wältken <clemens.waltken@microsoft.com>
2025-09-02 15:34:50 +0200

    Remove environment specific config

--- backend.tf
+++ backend.tf
@@ -1,8 +1,4 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "default"
-    storage_account_name = "tfstatecwcontoso"
-    container_name       = "tfstate"
-    key                  = "eon-september-dev.tfstate"
   }
 }
